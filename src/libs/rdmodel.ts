e38e4300d948593fec7ec704d41f03a46457025b
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2023-10-08 20:44:13 +0200

    load shader from assets

--- src/libs/rdmodel.ts
+++ src/libs/rdmodel.ts
@@ -1,3 +1,5 @@
+import shaderString from '../assets/shaders/cellShader.wgsl?raw'
+
 export default class ReactionDiffusionModel {
     
     canvas: HTMLCanvasElement
@@ -33,18 +35,7 @@ export default class ReactionDiffusionModel {
         
         this.cellShaderModule = device.createShaderModule({
             label: "Cell shader",
-            code: `
-            @vertex
-            fn vertexMain(@location(0) pos: vec2f) ->
-            @builtin(position) vec4f {
-                return vec4f(pos, 0, 1);
-            }
-            
-            @fragment
-            fn fragmentMain() -> @location(0) vec4f {
-                return vec4f(1, 0, 0, 1);
-            }
-            `
+            code: shaderString
         });
     }
     
